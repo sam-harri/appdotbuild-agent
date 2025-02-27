aa2b5d4ef039ccb9f709f50559f8a7eb59289ed7
David Gomes <david@neon.tech>
2025-02-27 17:16:57 +0100

    Increases agent server request timeouts (#42)

--- agent/main.py
+++ agent/main.py
@@ -45,7 +45,8 @@ def start_app() -> None:
         host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", "8080")),
         access_log=False,
-        reload=os.getenv("RELOAD", "false").lower() == "true"
+        reload=os.getenv("RELOAD", "false").lower() == "true",
+        timeout_keep_alive=60 * 20 * 1000, # 20 minutes
     )
 
 
