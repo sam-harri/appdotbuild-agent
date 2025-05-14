58dcdb23b4e122fe633fb1a084ecf7e4069c7544
Arseny Kravchenko <arsenyinfo@users.noreply.github.com>
2025-05-14 20:00:04 +0200

    Remove outdated files no longer used in the codebase (#195)

--- agent/tests/test_fsm_tools.py
+++
@@ -1,7 +0,0 @@
-from api.fsm_tools import run_main as run_main_fsm_tools
-from api.agent_server.agent_api_client import DEFAULT_APP_REQUEST
-
-
-def test_fsmtools_e2e():
-    messages = run_main_fsm_tools(DEFAULT_APP_REQUEST)
-    assert len(messages) > 1
