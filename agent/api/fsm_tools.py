69ff83cb8ecf2b1903b23331559d6a56ace0fe46
Eugene K <evgenii@neon.tech>
2025-06-26 12:35:44 +0100

    Dont duplicate agent internal message since it will anyway be sent

--- agent/api/fsm_tools.py
+++ agent/api/fsm_tools.py
@@ -315,7 +315,6 @@ class FSMToolProcessor:
             match block:
                 case TextRaw(text):
                     logger.info(f"LLM Message: {text}")
-                    await self.event_callback(f"🤔 Agent's thoughts:\n{text}")
                 case ToolUse(name):
                     match self.tool_mapping.get(name):
                         case None:
